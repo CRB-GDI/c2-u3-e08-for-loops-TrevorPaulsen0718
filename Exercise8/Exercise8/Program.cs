48cefd017d63ff7a6af28879b4e89da03e333737
TrevorPaulsen0718 <145356163+TrevorPaulsen0718@users.noreply.github.com>
2024-02-29 09:15:43 -0500

    Problem 1 Completed

---
+++ Exercise8/Exercise8/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Max Number ");
+
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i+=2);
+            {
+                Console.WriteLine ( number);
+            }
+        }
+    }
+}
