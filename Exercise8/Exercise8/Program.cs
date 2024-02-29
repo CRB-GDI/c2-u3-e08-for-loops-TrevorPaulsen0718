8487668b539dc7d8a323bad830c3acfe6ff77d88
TrevorPaulsen0718 <145356163+TrevorPaulsen0718@users.noreply.github.com>
2024-02-29 10:19:23 -0500

    Save

--- Exercise8/Exercise8/Program.cs
+++ Exercise8/Exercise8/Program.cs
@@ -4,13 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Max Number ");
+            Console.Write("Max Number = ");
 
             int number = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < number; i+=2);
+            
+            for (int i=0; i < number; i+=2)
             {
-                Console.WriteLine ( number);
+                Console.WriteLine(i);
+
+
+
+             //Problem 2
+
+            Console.WriteLine("Width = ");
+
+                int asterisks = int.Parse(Console.ReadLine());
+
+                for 
+
             }
         }
     }
